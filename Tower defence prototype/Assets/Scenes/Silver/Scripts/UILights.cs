5d66887f4e69078045b622c7ffefdd4519b0b512
CarrotXX2 <silver.bgame@gmail.com>
2024-10-15 10:12:39 +0200

    Getexturden main menu en script changes

--- Tower defence prototype/Assets/Scenes/Silver/Scripts/UILights.cs
+++ Tower defence prototype/Assets/Scenes/Silver/Scripts/UILights.cs
@@ -1,22 +1,19 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LightIntensitySlider : MonoBehaviour
+public class UILights : MonoBehaviour
 {
-    public Light targetLight;   // Sleep hier je licht object in
-    public Slider intensitySlider;  // Sleep hier de UI slider in
+    public Light targetLight; 
+    public Slider intensitySlider; 
 
     void Start()
     {
-        // Controleer of er een slider en light zijn toegewezen
         if (targetLight != null && intensitySlider != null)
         {
-            // Koppel de slider waarde aan de intensiteit van het licht
             intensitySlider.onValueChanged.AddListener(UpdateLightIntensity);
         }
     }
 
-    // Functie die wordt aangeroepen wanneer de slider waarde verandert
     public void UpdateLightIntensity(float value)
     {
         targetLight.intensity = value;
