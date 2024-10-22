cf2b6db172ff1768a5736755433c672d5abdce93
CarrotXX2 <silver.bgame@gmail.com>
2024-10-22 10:07:24 +0200

    Add voor settings en changes :P

---
+++ Tower defence prototype/Assets/Scenes/Silver/Scripts/SettingsScripts/ResolutionChange.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ResolutionChange : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+}
--- Tower defence prototype/Assets/Scripts/Base/BaseHP.cs
+++ Tower defence prototype/Assets/Scripts/Base/BaseHP.cs
@@ -9,23 +9,36 @@ public class BaseHP : BuildingHp
 {
     public TMP_Text healthText;
     public GameObject loseScreen;
+    public Image bloodyOverlay; 
+    public float maxOpacity = 0.8f;
+    public float opacityIncreaseRate = 0.1f; 
 
     public override void TakeDamage(float damage)
     {
         currentHealth -= damage;
         UpdateHealth(currentHealth);
-        
-        print("The Base took" + damage + " damage");
+
+        print("The Base took " + damage + " damage");
+
+        if (bloodyOverlay != null)
+        {
+            Color overlayColor = bloodyOverlay.color;
+            overlayColor.a += opacityIncreaseRate * damage;
+            overlayColor.a = Mathf.Clamp(overlayColor.a, 0, maxOpacity); 
+            bloodyOverlay.color = overlayColor;
+        }
+
         if (currentHealth <= 0)
         {
             DIE();
         }
     }
-    
-    void UpdateHealth(float currenthealth)
+
+    void UpdateHealth(float currentHealth)
     {
-        healthText.text = currenthealth.ToString() + "+";
+        healthText.text = currentHealth.ToString() + "+";
     }
+
     public override void DIE()
     {
         if (loseScreen != null)
@@ -36,11 +49,10 @@ public class BaseHP : BuildingHp
         {
             print("loseScreen is null");
         }
-        
-        // Time.timeScale = 0;
+
         enemyList.DestroyAllEnemies();
         enemyManager.gamePaused = true;
-        
+
         Destroy(gameObject);
     }
 }
